2fbc814ac49ac4e51dee3e128e51425067206ff9
Woo Song <ws392@drexel.edu>
2020-04-29 22:00:19 -0400

    push build

---
+++ wk3/part-2/build/precache-manifest.12c6d5255bb7b52c4ff8504f5761ed6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc1ebca196f4af871cedffde51ede07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cb7dde5953bba8d40a1",
+    "url": "/static/css/main.9d412c2d.chunk.css"
+  },
+  {
+    "revision": "df637600caf7bae6c9f0",
+    "url": "/static/js/2.cc8bb4cb.chunk.js"
+  },
+  {
+    "revision": "4c63396e008ab8ea46c17b570cef6624",
+    "url": "/static/js/2.cc8bb4cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb7dde5953bba8d40a1",
+    "url": "/static/js/main.fe8998b2.chunk.js"
+  },
+  {
+    "revision": "551f8d6318b83b568200",
+    "url": "/static/js/runtime-main.0e792e19.js"
+  }
+]);
